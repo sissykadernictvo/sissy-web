0c8d9ba4808d00fbd7665d18cbbf997fe4fe362e
Nikolas Póda <proformconsulting@gmail.com>
2026-04-14 16:17:43 +0200

    Fix sitemap urls

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,20 +1,24 @@
 import type { MetadataRoute } from "next";
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const now = new Date();
-
   return [
     {
       url: "https://www.sissy.sk",
-      lastModified: now,
+      lastModified: new Date(),
       changeFrequency: "weekly",
       priority: 1,
     },
     {
       url: "https://www.sissy.sk/cennik",
-      lastModified: now,
+      lastModified: new Date(),
       changeFrequency: "weekly",
       priority: 0.9,
+    },
+    {
+      url: "https://www.sissy.sk/ochrana-osobnych-udajov",
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.6,
     },
   ];
 }
