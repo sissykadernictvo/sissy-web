85c4859af90679e0f88b8bfae4c798c6d4f672b9
Nikolas Póda <proformconsulting@gmail.com>
2026-04-29 12:22:07 +0200

    Add Microsoft Clarity

--- app/layout.tsx
+++ app/layout.tsx
@@ -101,6 +101,17 @@ export default function RootLayout({
             gtag('config', 'G-XFFQ84C898');
           `}
         </Script>
+
+        <Script id="microsoft-clarity" strategy="afterInteractive">
+          {`
+            (function(c,l,a,r,i,t,y){
+                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "wj81u829yb");
+          `}
+        </Script>
+
         {children}
       </body>
     </html>
